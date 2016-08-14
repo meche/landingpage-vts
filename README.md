191bc2891ed1cdd1428f20ded72dcaac591a847e
Leandro Alexandrino <backmelt@gmail.com>
2016-08-14 00:29:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+![Warrior Tank](logo.png)
+
+[![Build Status](https://travis-ci.org/meche/warrior-tank.svg?branch=master)](https://travis-ci.org/meche/warrior-tank)
+
+The boilerplate for frontend.
+
+This boilerplate is a simple example used on my [workflow](https://github.com/meche/workflow).
+
+## Install
+
+1. Fork this repo;
+2. install `nodejs`;
+3. Clone in your environment the repo forked;
+4. npm install.
+
+## Usage
+
+run in your terminal `gulp pullTheBoss` for watch.
---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+'use strict'
+
+var gulp = require('gulp');
+var autoprefixer = require( 'autoprefixer-stylus' );
+var browserSync = require( 'browser-sync' );
+var clean = require( 'gulp-clean' );
+var concat = require( 'gulp-concat' );
+var htmlMin = require( 'gulp-htmlmin' );
+var imagemin = require( 'gulp-imagemin' );
+var koutoSwiss = require( 'kouto-swiss' );
+var jade = require( 'gulp-jade' );
+var jeet = require( 'jeet' );
+var jsHint = require( 'gulp-jshint' );
+var map = require( 'map-stream' );
+var plumber = require('gulp-plumber');
+var rename = require( 'gulp-rename' );
+var rupture = require( 'rupture' );
+var sourceMaps = require( 'gulp-sourcemaps' );
+var stylus = require ( 'gulp-stylus' );
+var uglify = require( 'gulp-uglify' );
+
+var jadeForest = {
+    appJs: 'app/**/*.js',
+    css: 'assets/stylesheet/**/*.styl',
+    cssStylus: 'assets/stylesheet/styles.styl',
+    html: 'dev/**/*.html',
+    img: 'assets/images/**/*',
+    jade: 'assets/**/*.jade',
+    js: 'assets/javascript/**/*.js'
+};
+
+var kunlaiSummit = {
+    css: 'dev/css/',
+    dev: 'dev/',
+    img: 'dev/images/',
+    js: 'dev/js/'
+};
+
+var eternalBlossoms = {
+    css: 'dist/css/',
+    img: 'dist/img/',
+    js: 'dist/js/',
+    dist: 'dist/'
+};
+
+var myReporter = map(function (file, cb) {
+  if (!file.jshint.success) {
+    console.log('JSHINT fail in '+file.path);
+    file.jshint.results.forEach(function (err) {
+      if (err) {
+        console.log(' '+file.path + ': line ' + err.line + ', col ' + err.character + ', code ' + err.code + ', ' + err.reason);
+      }
+    });
+  }
+  cb(null, file);
+});
+
+// Clear folder dist
+
+gulp.task( 'clean', function() {
+    return gulp.src( jadeForest.html )
+        .pipe( clean() );
+} );
+
+// compiler css with Stylus
+
+gulp.task( 'css', function() {
+    gulp.src( jadeForest.cssStylus )
+        .pipe( plumber() )
+        .pipe( sourceMaps.init() )
+        .pipe( stylus( {
+            use: [koutoSwiss(), autoprefixer('> 0%'), jeet(), rupture()],
+            compress: false
+        } ) )
+        .pipe( sourceMaps.write( './' ) )
+        .pipe( plumber.stop() )
+        .pipe( gulp.dest( kunlaiSummit.css ) )
+        .pipe( browserSync.reload( {
+            stream: true
+        } ) );
+} );
+
+// compiler jade
+
+gulp.task( 'jade', function() {
+    gulp.src( jadeForest.jade )
+        .pipe( jade( {
+            pretty: true
+        } ) )
+        .pipe( gulp.dest( kunlaiSummit.dev ) );
+} );
+
+// lint javascript
+
+gulp.task( 'lint', function() {
+  return gulp.src( jadeForest.js )
+    .pipe( jsHint() )
+    .pipe( myReporter );
+} );
+
+// Image Min
+
+gulp.task( 'image', function() {
+  gulp.src( jadeForest.img )
+    .pipe( imagemin() )
+    .pipe( gulp.dest( kunlaiSummit.img ) )
+    .pipe( browserSync.reload( {
+        stream: true
+    } ) );
+} );
+
+// browserSync
+
+gulp.task( 'browserSync', function() {
+    browserSync( {
+        server: {
+            baseDir: kunlaiSummit.dev
+        }
+    } )
+} );
+
+// Watch files... PULL THE BOSS!!
+
+gulp.task( 'pullTheBoss', ['css', 'jade', 'image', 'browserSync'], function() {
+    gulp.watch( jadeForest.css, ['css'] );
+    gulp.watch( jadeForest.jade, ['jade'] );
+    gulp.watch( jadeForest.img, ['image'] );
+    gulp.watch( jadeForest.html, browserSync.reload );
+} );
