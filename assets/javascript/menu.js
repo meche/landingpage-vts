6744d916d61d7b03687d7e67e2058153eaae4eb4
Leandro Alexandrino <backmelt@gmail.com>
2016-08-15 00:50:26 -0300

    fix menu responsive

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,5 +1,22 @@
 (function (){
+	'use strict'
 
+	var menu = document.querySelector('.menu-list')
 
+	menu.addEventListener('click', function (element) {
+		if (menu.classList.contains('hidden')) {
+		  // The box that we clicked has a class of bad so let's remove it and add the good class
+		  setTimeout(function () {
+			  menu.classList.remove('hidden')
+			  menu.classList.add('active')
+		  }, 250)
+
+		} else {
+			setTimeout(function () {
+				menu.classList.remove('active')
+				menu.classList.add('hidden')
+			}, 250)
+		}
+	})
 
 })()
--- dev/js/main.js
+++ dev/js/main.js
@@ -1,2 +1,2 @@
-
+!function(){"use strict";var t=document.querySelector(".menu-list");t.addEventListener("click",function(e){t.classList.contains("hidden")?setTimeout(function(){t.classList.remove("hidden"),t.classList.add("active")},250):setTimeout(function(){t.classList.remove("active"),t.classList.add("hidden")},250)})}();
 //# sourceMappingURL=main.js.map
